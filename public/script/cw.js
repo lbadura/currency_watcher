e6425ff7bf02281909b7ae82ae2c51f6e4e25a0a
Łukasz Badura <lukasz@niebo.net>
2011-05-24 23:12:13 +0200

    Indicate which currency are we showing.

--- public/script/cw.js
+++ public/script/cw.js
@@ -1,2 +1,11 @@
+cw = {}
+
+cw.killActiveMenuClick = function ( ev ) {
+  ev.stopPropagation();
+  ev.preventDefault();
+  return false;
+}
+
 $(document).ready(function() {
+  $('ul a.active').live('click', cw.killActiveMenuClick);
 });
